3f13201483afac8fcc001a3b8a3e85f989b689e5
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2017-12-28 16:43:32 -0500

    Update QLearner with botworld unit test

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,36 @@
 /// ```
 /// use qlearn::QLearner;
 ///
-/// let learner = QLearner::new(100, 5);
+/// let (state, last_state, action, reward, update) = (99, 98, 1, 0.0, true);
+///
+/// let mut learner = QLearner::new(100, 5);
+/// let next_action = learner.query(state, last_state, action, reward, update);
+///
+/// println!("QLearner selected action: {}", next_action);
 /// ```
 extern crate rand;
 
 use rand::distributions::{IndependentSample, Range};
 
+mod macros;
 
 /// The `QLearner` type. See [the module level documentation](index.html) for more.
 pub struct QLearner {
+    /// Number of states
     pub n_states: usize,
+    /// Number of actions
     pub n_actions: usize,
+    /// Learning rate
     pub alpha: f32,
+    /// Discount factor
     pub gamma: f32,
+    /// Initial randomness 
     pub rar: f32,
+    /// Randomness decay rate
     pub radr: f32,
+    /// Table of Q values
     pub q_table: Vec<Vec<f32>>,
+    /// Random number generator
     pub rng: rand::ThreadRng,
 }
 
@@ -43,15 +57,21 @@ impl QLearner {
 
     /// This is the primary method for querying and training the learner. Use `update = true` when training.
     pub fn query(&mut self, s_curr: usize, s_prev: usize, action: usize, reward: f32, update: bool) -> usize {
+        debug!("self.q_table[scurr]: {:?}", &self.q_table[s_curr]);
         let mut next_action = argmax(&self.q_table[s_curr]);
         if update == true {
-            self.q_table[s_prev][action] = (1.0 - self.alpha) * self.q_table[s_prev][action] + self.alpha * (reward + self.gamma * self.q_table[s_curr][action])
+            self.q_table[s_prev][action] = 
+                (1.0 - self.alpha) 
+                * self.q_table[s_prev][action] 
+                + self.alpha 
+                * (reward + self.gamma * self.q_table[s_curr][action])
         }
         // Inject some randomness into choice of action
         let prob = Range::new(0.0, 1.0);
         let rprob = prob.ind_sample(&mut self.rng);
         if rprob < self.rar {
-            let ractions = Range::new(0, self.n_actions);
+            debug!("Random action ({} < {}).", rprob, self.rar);
+            let ractions = Range::new(0, self.n_actions-1);
             next_action = ractions.ind_sample(&mut self.rng);
         }
 
@@ -59,10 +79,8 @@ impl QLearner {
         if update == true {
             self.rar = self.rar * self.radr
         }
-
         next_action
     }
-
 }
 
 
@@ -70,12 +88,14 @@ impl QLearner {
 fn argmax(v: &Vec<f32>) -> usize {
     let mut max_val : f32 = std::f32::NEG_INFINITY; 
     let mut max_idx : usize = 0;
+    debug!("v.len: {}", v.len());
     for i in 0..v.len() {
         if v[i] > max_val {
             max_val = v[i];
             max_idx = i;
         }
     }
+    debug!("argmax: {}", max_idx);
     max_idx
 }
     
@@ -83,14 +103,15 @@ fn argmax(v: &Vec<f32>) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    mod botworld;
 
     #[test]
     fn qlearner_walk() {
-        let mut q = QLearner::new(100, 5);
+        let mut q = QLearner::new(100, 4);
         let original_q_table = q.q_table.clone();
         let mut state = 0;
         let mut prev_state = 1;
-        let mut action = 4;
+        let mut action = 3;
         let reward = 1.0;
         for _ in 0..10000 {
             action = q.query(state, prev_state, action, reward, true);
@@ -101,9 +122,15 @@ mod tests {
         for i in 0..q.n_states {
             for j in 0..q.n_actions {
                 if original_q_table[i][j] != q.q_table[i][j] {
-                    println!("Different value at ({},{})", i, j)
+                    debug!("Different value at ({},{})", i, j);
                 }
             }
         }
+    }
+
+    #[test]
+    fn qlearner_test() {
+        use tests::botworld::test_learner;
+        test_learner()
     }
 }
